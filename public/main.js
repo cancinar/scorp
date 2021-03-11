d00b4b862d45fffbedcff56024eebef67ce1588a
Can Cinar <cancinar@outlook.com>
2021-03-11 21:30:35 +0300

    initial commit

---
+++ public/main.js
@@ -0,0 +1,80 @@
+// @ts-check
+"use strict";
+import { APIWrapper, API_EVENT_TYPE } from "./api.js";
+import {
+  addMessage,
+  animateGift,
+  isPossiblyAnimatingGift,
+  isAnimatingGiftUI,
+} from "./dom_updates.js";
+
+const api = new APIWrapper();
+let interval;
+class EventQueue {
+  queue;
+  processingEvent;
+  static priorities = new Map([
+    ["ag", 1],
+    ["m", 2],
+    ["g", 3],
+  ]);
+
+  constructor() {
+    this.queue = [];
+    this.processingEvent = {};
+  }
+
+  add(items) {
+    this.queue.push(...items);
+    this.queue.sort(this.compare);
+  }
+
+  compare(a, b) {
+    if (EventQueue.priorities.get(a.type) < EventQueue.priorities.get(b.type)) {
+      return -1;
+    }
+    if (EventQueue.priorities.get(a.type) > EventQueue.priorities.get(b.type)) {
+      return 1;
+    }
+    return 0;
+  }
+
+  getFirst() {
+    return this.queue[0];
+  }
+
+  consume() {
+    this.processingEvent = this.queue[0];
+    return this.queue.shift();
+  }
+}
+
+const queue = new EventQueue();
+
+function replay() {
+  if (typeof queue.getFirst() === "undefined") {
+    return;
+  }
+  document.getElementById(
+    "animatedGift"
+  ).innerHTML = queue.consume().data.gift_emoji;
+  document.getElementById("messages").innerHTML = queue.consume().data.message;
+}
+
+function init() {
+  setInterval(() => {
+    if (queue.processingEvent.type === "ag") {
+      return;
+    }
+
+    replay();
+  }, 500);
+}
+
+init();
+
+api.setEventHandler((events) => {
+  queue.add(events);
+});
+
+// NOTE: UI helper methods from `dom_updates` are already imported above.
